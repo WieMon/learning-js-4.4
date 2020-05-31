620486e9cac350253fc2fe6d17b7995fbe461354
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-31 19:53:51 +0200

    Add darkening and brightening on arrow up and down

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let red = 100;
+let green = 100;
+let blue = 100;
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+const changeColor = (e) => {
+  console.log(e.keyCode, e.which);
+
+  // if (e.keyCode === 38 && red < 255) {
+  //   console.log('strzalka gora')
+  //   red += 1;
+  //   green += 1;
+  //   blue += 1;
+  //   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  // } else if (e.keyCode ===40) {
+  //   console.log('strzalka dol')
+  //   red--;
+  //   green--;
+  //   blue--;
+  //   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  // }
+
+  switch (e.keyCode) {
+    case 38:
+      document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red}, ${green < 255 ? ++green : green}, ${blue < 255 ? ++blue : blue})`;
+      break;
+    case 40:
+      document.body.style.backgroundColor = `rgb(${red > 0 ? --red : red}, ${green > 0 ? --green : green}, ${blue > 0 ? --blue : blue})`;
+      break;
+  }
+  
+}
+
+window.addEventListener('keydown', changeColor)
